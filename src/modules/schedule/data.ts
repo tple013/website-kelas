61a6545dc453a6a37149deccdfcaa46154e4a0c1
TPLE013 <akun.tple013@gmail.com>
2026-01-31 14:36:11 +0700

    Update schedule data with lorem ipsum placeholder

--- src/modules/schedule/data.ts
+++ src/modules/schedule/data.ts
@@ -1,13 +1,13 @@
 import type { ScheduleItem } from "./types";
 
 export const schedules: ScheduleItem[] = [
-  { id: "1", day: "Senin", time: "08:00 - 09:40", subject: "Lorem Ipsum Dolor", location: "Ruang A.101", instructor: "Dr. Lorem Ipsum, M.Sc" },
-  { id: "2", day: "Senin", time: "10:00 - 11:40", subject: "Sit Amet Consectetur", location: "Ruang A.102", instructor: "Prof. Adipiscing Elit" },
-  { id: "3", day: "Selasa", time: "13:00 - 14:40", subject: "Sed Do Eiusmod", location: "Ruang B.201", instructor: "Tempor Incididunt, M.Pd" },
-  { id: "4", day: "Selasa", time: "15:00 - 16:40", subject: "Ut Labore Dolore", location: "Ruang B.202", instructor: "Magna Aliqua, Ph.D" },
-  { id: "5", day: "Rabu", time: "08:00 - 10:00", subject: "Praktikum Enim Minim", location: "Lab Lorem 1", instructor: "Asisten Veniam" },
-  { id: "6", day: "Kamis", time: "09:00 - 10:40", subject: "Quis Nostrud Exercitation", location: "Ruang C.301", instructor: "Ullamco Laboris, M.Ed" },
-  { id: "7", day: "Kamis", time: "13:00 - 14:40", subject: "Nisi Ut Aliquip", location: "Ruang C.302", instructor: "Dr. Ex Ea Commodo" },
-  { id: "8", day: "Jumat", time: "08:00 - 11:00", subject: "Duis Aute Irure", location: "Ruang D.401", instructor: "Ir. Dolor Reprehenderit" },
-  { id: "9", day: "Sabtu", time: "08:00 - 12:00", subject: "Workshop Voluptate Velit", location: "Lab Lorem 2", instructor: "Tim Cillum Dolore" },
+  { id: "1", day: "Senin", time: "08:00 - 09:40", subject: "Lorem Ipsum Dolor", location: "Ruang V.114", instructor: "Dr. Lorem Ipsum" },
+  { id: "2", day: "Senin", time: "10:00 - 11:40", subject: "Sit Amet Consectetur", location: "Ruang V.114", instructor: "Adipiscing Elit" },
+  { id: "3", day: "Selasa", time: "13:00 - 14:40", subject: "Sed Do Eiusmod", location: "Ruang V.114", instructor: "Tempor Incididunt" },
+  { id: "4", day: "Selasa", time: "15:00 - 16:40", subject: "Ut Labore Dolore", location: "Ruang V.114", instructor: "Magna Aliqua" },
+  { id: "5", day: "Rabu", time: "08:00 - 10:00", subject: "Praktikum Enim Minim", location: "Ruang V.114", instructor: "Veniam" },
+  { id: "6", day: "Kamis", time: "09:00 - 10:40", subject: "Quis Nostrud Exercitation", location: "Ruang V.114", instructor: "Ullamco Laboris" },
+  { id: "7", day: "Kamis", time: "13:00 - 14:40", subject: "Nisi Ut Aliquip", location: "Ruang V.114", instructor: "Ex Ea Commodo" },
+  { id: "8", day: "Jumat", time: "08:00 - 11:00", subject: "Duis Aute Irure", location: "Ruang V.114", instructor: "Dolor Reprehenderit" },
+  { id: "9", day: "Sabtu", time: "08:00 - 12:00", subject: "Workshop Voluptate Velit", location: "Lab Lorem 2", instructor: "Cillum Dolore" },
 ];
